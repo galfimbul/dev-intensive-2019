08e26fe0477f432eee064943670b9ede7dbf9a42
Alexandr <aevshvetsov@gmail.com>
2019-07-02 02:37:17 +0300

    make transliteration function

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -11,7 +11,58 @@ object Utils {
     }
 
     fun transliteration(payload: String, divider:String = " "): String {
-        return "LOL"
+
+        var letter:String = ""
+        var result = StringBuilder()
+        val translit = mapOf(
+        "а" to "a",
+        "б" to "b",
+        "в" to "v",
+        "г" to "g",
+        "д" to "d",
+        "е" to "e",
+        "ё" to "e",
+        "ж" to "zh",
+        "з" to "z",
+        "и" to "i",
+        "й" to "i",
+        "к" to "k",
+        "л" to "l",
+        "м" to "m",
+        "н" to "n",
+        "о" to "o",
+        "п" to "p",
+        "р" to "r",
+        "с" to "s",
+        "т" to "t",
+        "у" to "u",
+        "ф" to "f",
+        "х" to "h",
+        "ц" to "c",
+        "ч" to "ch",
+        "ш" to "sh",
+        "щ" to "sh'",
+        "ъ" to "",
+        "ы" to "i",
+        "ь" to "",
+        "э" to "e",
+        "ю" to "yu",
+        "я" to "ya"
+        )
+
+        for (symbol in 0..payload.length-1){
+            if ("${payload[symbol]}" in translit){
+                result.append(translit.get("${payload[symbol]}"))
+            }
+            else if("${payload[symbol].toLowerCase()}" in translit){
+                    result.append(translit.get("${payload[symbol].toLowerCase()}")?.capitalize())
+                }
+            else if (!translit.containsValue("${payload[symbol].toLowerCase()}")){result.append(divider)}
+            else if (translit.containsValue("${payload[symbol].toLowerCase()}")){result.append("${payload[symbol]}")}
+
+        }
+
+        return result.toString()
 
     }
 
