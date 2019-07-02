bd643980e6a507faee11346cc859da77043fd079
Alexandr <aevshvetsov@gmail.com>
2019-07-03 00:34:13 +0300

    debug BaseMessage,parseFullName and complite hometask2

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,11 +2,11 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
     fun parseFullName(fullName:String?):Pair<String?,String?>{
-        val parts:List<String>? = fullName?.split(" ")
+                val parts:List<String>? = fullName?.trimStart()?.trim()?.split(" ")
         var firstName = parts?.getOrNull(0)
-        var lastName = parts?.getOrNull(1)
+        var lastName = parts?.getOrNull(parts.size-1)
         if (firstName.isNullOrBlank()) firstName = null
-        if (lastName.isNullOrBlank()) lastName = null
+        if (lastName.isNullOrBlank()||firstName.equals(lastName)) lastName = null
         return firstName to lastName
     }
 
