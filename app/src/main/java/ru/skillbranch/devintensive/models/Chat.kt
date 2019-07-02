a6288333455c8c432a0108fcac9aa00bb5d093d6
Alexandr <aevshvetsov@gmail.com>
2019-07-02 22:02:52 +0300

    make Base Message, AbstractFactory

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Chat.kt
@@ -0,0 +1,11 @@
+package ru.skillbranch.devintensive.models
+
+/**
+ * Created by Alexander Shvetsov on 02.07.2019
+ */
+class Chat(
+    val id:String,
+    val members:MutableList<User> = mutableListOf(),
+    val messages:MutableList<BaseMessage> = mutableListOf()
+) {
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/models/ImageMessage.kt
@@ -0,0 +1,19 @@
+package ru.skillbranch.devintensive.models
+
+import ru.skillbranch.devintensive.extension.humanizeDiff
+import java.util.*
+
+/**
+ * Created by Alexander Shvetsov on 02.07.2019
+ */
+class ImageMessage(
+    id:String,
+    from:User?,
+    chat:Chat,
+    isIncoming:Boolean = false,
+    date: Date = Date(),
+    var image:String?
+): BaseMessage(id, from, chat, isIncoming, date) {
+    override fun formatMessage(): String = "id:$id ${from?.firstName}" +
+            "${if (isIncoming) " получил" else " отправил" } изображение \"$image\" ${date.humanizeDiff()}"
+}
