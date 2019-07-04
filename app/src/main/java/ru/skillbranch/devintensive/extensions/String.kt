6ff4479956503031eeaca521621603bac65d7028
Alexandr <aevshvetsov@gmail.com>
2019-07-04 14:04:21 +0300

    debug truncate with bot tests fix

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -5,15 +5,11 @@ package ru.skillbranch.devintensive.extensions
  */
 
 fun String.truncate(value:Int=16):String{
-    if(this.length-1<=value)
-        return this
-    val result = this.trimStart().substring(0,value+1)
-    if (result[result.length-1].isWhitespace()){
-        return result.trim()}
-    if(value+2<=this.length-1&&this.trimStart()[value+2].isWhitespace()){
+    val result = this.trim()
+    if(result.length<=value){
         return result
     }
-    else return "$result..."
+    else return "${result.substring(0,value).trim()}..."
 
 }
 
